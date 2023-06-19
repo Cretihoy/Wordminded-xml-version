8c5bdfbeb2f00af19add9672e2cf95502683bfd0
cretihoy <sergzhio238@gmail.com>
2023-06-19 21:24:32 +0300

    create settings activity

---
+++ app/src/main/java/com/example/wordmindedxmlversion/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wordmindedxmlversion.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wordmindedxmlversion.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
