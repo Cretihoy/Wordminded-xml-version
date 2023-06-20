5aea07cf1dc00f0af7a6183068924acb4d94a103
cretihoy <sergzhio238@gmail.com>
2023-06-20 14:55:56 +0300

    create end game activity and add components in xml

---
+++ app/src/main/java/com/example/wordmindedxmlversion/end/EndGameActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wordmindedxmlversion.end
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wordmindedxmlversion.R
+
+class EndGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_game)
+    }
+}
